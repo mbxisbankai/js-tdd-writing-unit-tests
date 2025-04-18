6f1d490a09d3a33ef673e03e38ca740fbec4c0bf
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-04-18 16:08:30 +0300

    TDD Writing Unit Tests

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+function pointsForWord(word){
+    let points = 0;
+    for (const char of word) {
+        points += /[aeiou]/.test(char) ? 1 : 2;
+    }
+    return points;
+}
+
+export default pointsForWord;
